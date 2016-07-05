344ba48809578886e5a8b8eb00c2241c6cf28376
Monica Mow <mow.monica@gmail.com>
2016-07-05 16:42:05 -0700

    Check for error messages in the request callback
    - log error message to console if error
    - only log html to console if no error and status code 200

--- sub_stack.js
+++ sub_stack.js
@@ -2,5 +2,11 @@ var request = require('request')
 var cheerio = require('cheerio')
 
 request('http://substack.net/images/', function (error, response, html) {
-  console.log(html);
+  if (error)
+    console.log(error)
+
+  if (!error && response.statusCode == 200) {
+    console.log(html);
+  }
+  
 });

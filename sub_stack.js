36365b1695f215f461e83ffe481a391e35426015
Monica Mow <mow.monica@gmail.com>
2016-07-05 17:04:11 -0700

    Parse the HTML by targeting each table row
    - get each table row and get the first, second and third child, which are the three cells (td's) in each table row
      > these contain the file permission, absolute url and filetype
    - get the text contained in each td

--- sub_stack.js
+++ sub_stack.js
@@ -6,7 +6,12 @@ request('http://substack.net/images/', function (error, response, html) {
     console.log(error)
 
   if (!error && response.statusCode == 200) {
-    console.log(html);
+    var $ = cheerio.load(html);
+    $('tr').each(function(i, element) { 
+      var filePermission = $(this).children(':nth-child(1)').text();
+      var absoluteURL = $(this).children(':nth-child(2)').text();
+      var fileType = $(this).children(':nth-child(3)').text();
+    });
   }
   
 });
